c3977dd2bff847ad64d23dbb277bfa1fcd73e9f9
Logan Kloft <lkloft@outlook.com>
2023-08-11 20:06:03 -0700

    Complete contact page.

---
+++ src/contact.js
@@ -0,0 +1,5 @@
+import './contact.css';
+
+export default (content) => {
+    content.innerHTML = '<div><h3>Owner</h3><p>phone: (333)-444-5555</p><p>email: mldiner@gmail.com</p></div><div><h3>Restaurant</h3><p>phone: (333)-444-5555</p><p>email: mldiner@gmail.com</p><p>address: Dark side of the moon</p></div><div><p>Apply by sending resume to mldiner@gmail.com</p></div>';
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -15,7 +17,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ]
   }
 };
